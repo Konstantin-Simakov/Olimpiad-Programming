9a46fc997c631c23258d03bbb3b7eafc8745d2bb
Konstantin_Simakov <kvs.2000@mail.ru>
2024-06-08 12:21:01 +0300

    Add files via upload

--- Python/Classwork/CW_6/grpah.py
+++ Python/Classwork/CW_6/grpah.py
@@ -4,15 +4,22 @@
 from collections import deque
 
 
+# With start vertice "you" (see below).
+vertices = 1
+edges = 0
+
 # BFS for graphs.
 def bfs_search(graph, first_person):  
     search_queue = deque()                              # Create a new queue.
     search_queue += graph[first_person]                 # All neighbours are added in search queue.
     searched = []                                       # Already checked persons.
     
+    global vertices, edges
     while search_queue:                                 # While queue is not empty...
         person = search_queue.popleft()                 # First person is pop from the queue.
+        edges += 1
         if not person in searched:
+            vertices += 1
             if is_seller(person):                       # Check if the person is mango seller.
                 print(person, "is a mango seller!")     # Yes, it is a mango seller.
                 return True                             # The mango seller is found.
@@ -43,3 +50,5 @@ graph["jonny"] = []
 # Launch search.
 if not bfs_search(graph, "you"):
     print("There are no mango seller.")
+
+print("\nThere are", vertices, "vertices and", edges, "edges in the graph.")
