41e9d05d8e2913816f7a2a8ff56ac1ed0cc483e0
Konstantin_Simakov <kvs.2000@mail.ru>
2024-08-11 14:12:55 +0300

    Add files via upload

--- Python/Classwork/CW_9/rucksack.py
+++ Python/Classwork/CW_9/rucksack.py
@@ -1,5 +1,7 @@
-# knapsack.py
+# rucksack.py
 # Solve the rucksack problem using dynamic programming method.
+
+MAX_WEIGHT = 4      # The max capacity of the rucksack in kg.
 
 
 def get_weights_costs(stuff):
@@ -11,13 +13,13 @@ def get_weights_costs(stuff):
     return weights, costs
 
 
-def get_memtable(stuff, max_weight=4):
+def get_memtable(stuff, max_weight=MAX_WEIGHT, step=1):
     """Get memoization table based on weigth and cost lists."""
     weights, costs = get_weights_costs(stuff)
     items = len(costs)                  # Number of table lines.
 
-    # Create the memoization table with oh costs.
-    memtable = [[0 for j in range(max_weight+1)] for i in range(items)]
+    # Create the memoization table with zero costs.
+    memtable = [[0 for j in range(0, max_weight+1, step)] for i in range(items)]
     print_memtable(memtable, items, max_weight+1)
     
     # Initialize it.
@@ -38,7 +40,7 @@ def get_memtable(stuff, max_weight=4):
     return memtable, weights, costs
 
 
-def get_selected_items(stuff, max_weight=4):
+def get_selected_items(stuff, max_weight=MAX_WEIGHT):
     """Make a list of your most valuable items with a rucksack weight limit."""
     memtable, weights, costs = get_memtable(stuff)
     print(weights)
@@ -46,17 +48,17 @@ def get_selected_items(stuff, max_weight=4):
     items = len(costs)                          # Number of table lines.
     print_memtable(memtable, items, max_weight+1)
 
-    weight = max_weight                         # Start from the maximum weight.
+    j = max_weight                              # Start from the maximum weight.
+    rest_cost = memtable[items-1][max_weight]   # Start with the last element of the table, its max cost.
     needed_items = []                           # The list of items as pairs: weight and cost.
 
-    res = memtable[items-1][max_weight]         # Start with the last element of the table.
     for i in range(items-1, 0, -1):             # The loop in reverse order through the list.
-        if res <= 0:                            # The rucksack is packed to the maximum.
+        if 0 == rest_cost:                      # The rucksack is packed to the maximum.
             break
-        elif res != memtable[i-1][weight]:      # Take an item.
+        elif rest_cost != memtable[i-1][j]:     # Take an item.
             needed_items.append((weights[i], costs[i]))
-            res -= costs[i]
-            weight -= weights[i]
+            rest_cost -= costs[i]
+            j -= weights[i]                     # Transfer to the previous max cost of the rest capacity.
 
     print(needed_items)
 
@@ -71,13 +73,13 @@ def get_selected_items(stuff, max_weight=4):
 
 
 def print_memtable(memtable, rows, cols):
-    for i in range(1, rows):
-        for j in range(1, cols):
-            print(memtable[i][j], end=" ")
+    for i in range(0, rows):
+        for j in range(0, cols):
+            print("%5d" % memtable[i][j], end=" ")
         print()
 
 
-# Here is a tuple where the first item means its weight
+# A dictionary with tuples where the first item means its weight
 # and second item means its cost (in conventional monetary units).
 stuff = {
         "guitar": (1, 1500),
@@ -88,8 +90,9 @@ stuff = {
 items_list = get_selected_items(stuff)
 print(items_list, "\n")
 
+print("The total capacity of the rucksack is", MAX_WEIGHT, "kg.")
 total_weight = sum([stuff[item][0] for item in items_list])
-print("The total weight of the rucksack is", total_weight, "kg.")
+print("The total weight of items in the rucksack is", total_weight, "kg.")
 
 total_cost = sum([stuff[item][1] for item in items_list])
-print("The total cost of the rucksack is", total_cost, "rubles.")
+print("The total cost of items in the rucksack is", total_cost, "rubles.")
