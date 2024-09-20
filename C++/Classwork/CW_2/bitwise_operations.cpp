888f54f2ef45497e3ca65a004a9195587da521cb
Konstantin_Simakov <kvs.2000@mail.ru>
2024-09-20 14:19:12 +0300

    Add files via upload

--- C++/Classwork/CW_2/bitwise_operations.cpp
+++ C++/Classwork/CW_2/bitwise_operations.cpp
@@ -5,62 +5,66 @@
 #include <climits>
 using namespace std;
 
-void dec_to_bin(int num);
+void to_bin(int num);               
+int builtin_clz(int num);           // The number of leading zeroes in BIN.
+int builtin_ctz(int num);           // The number of ending zeroes in BIN.
+int builtin_popcount(int num);      // The number of ones in BIN.
+bool builtin_parity(int num);       // Odd/even number of ones in BIN.
 
 int main(void)
 {
     // Program preparation.
-    const int K = 0;
-    string filler;
+    const int K = 2;
+    string suffix;
     if (K <= 0)
     {
         cout << "Entered incorrect bitwise shift (K <= 0). Program terminating.\n";
         exit(EXIT_FAILURE);
     }
     else if (K == 1)
-        filler = "st";
+        suffix = "st";
     else if (K == 2)
-        filler = "nd";
+        suffix = "nd";
     else if (K == 3)
-        filler = "rd";
+        suffix = "rd";
     else
-        filler = "th";
+        suffix = "th";
 
     // Source data input.
     int num;
     cout << "Enter an integer: ";
     cin >> num;
     cout << "Source number:\n";
-    dec_to_bin(num);
+    to_bin(num);
     
     cout << "\n\n(Bit numbering starts from zero, as for arrays, but from right to left.)\n\n";
 
     // Test bitwise operaions and functions.
     int res;
     cout << "1. Simple cases.\n";
-    cout << "Set the " << K << filler << " bit to 1:\n";
+    cout << "Set the " << K << suffix << " bit to 1:\n";
     res = num | (1 << K);
-    dec_to_bin(res);
-    cout << "\nReset the " << K << filler << " bit to 0:\n";
+    to_bin(res);
+    cout << "\nReset the " << K << suffix << " bit to 0:\n";
     res = num & ~(1 << K);
-    dec_to_bin(res);
-    cout << "\nInvert the " << K << filler << " bit:\n"; 
+    to_bin(res);
+    cout << "\nInvert the " << K << suffix << " bit:\n"; 
     res = num ^ (1 << K);
-    dec_to_bin(res);
+    to_bin(res);
 
     cout << "\n\n2. More difficult cases.\n";
     cout << "Reset the last one bit to 0:\n";
     res = num & (num - 1);
-    dec_to_bin(res);
+    to_bin(res);
     cout << "\nReset all one bits to 0 except the last one:\n";
     res = num & -num;
-    dec_to_bin(res);
+    to_bin(res);
     cout << "\nSet all bits after the last one to one:\n";
     res = num | (num - 1);
-    dec_to_bin(res);
+    to_bin(res);
 
     cout << "\n\n";
-    dec_to_bin(num);
+    to_bin(num);
     cout << " (" << num << ") ";
     if ((num & (num - 1)) == 0)
         cout << "is power of 2.\n";
@@ -68,30 +72,99 @@ int main(void)
         cout << "isn\'t power of 2.\n";
 
     cout << "\n3. Built-in functions.\n";
-    dec_to_bin(num);
+    to_bin(num);
     cout << " (" << num << ")\n";
     cout << "The number of leading zeros in binary representation: "
          << __builtin_clz(num) << endl;
+    cout << "The number of leading zeros in binary representation (my version): "
+         << builtin_clz(num) << endl;
     cout << "The number of ending zeros in binary representation: "
          << __builtin_ctz(num) << endl;
+    cout << "The number of ending zeros in binary representation (my version): "
+         << builtin_ctz(num) << endl;
     cout << "The number of ones in binary representation: "
          << __builtin_popcount(num) << endl;
+    cout << "The number of ones in binary representation (my version): "
+         << builtin_popcount(num) << endl;
     cout << "The number of ones in binary representation is ";
     if (__builtin_parity(num))
         cout << "odd.\n";
     else
         cout << "even.\n";
+    cout << "The number of ones in binary representation is ";
+    if (builtin_parity(num))
+        cout << "odd.";
+    else
+        cout << "even.";
+    cout << " (my version)\n";
 
     return 0;
 }
 
-void dec_to_bin(int num)
+void to_bin(int num)
 {
-    for (int k = sizeof(int) * CHAR_BIT - 1; k >= 0; --k)
+    for (int k = sizeof num * CHAR_BIT - 1; k >= 0; --k)
     {
         if (num & (1 << k))
             cout << '1';
         else
             cout << '0';
+        if (!(k & 3))
+            cout.put(' ');
     }
+    cout << '\b';
 }
+
+int builtin_clz(int num)
+{
+    int count = 0;
+    for (int k = sizeof num * CHAR_BIT - 1; k >= 0; --k)
+    {
+        if (!(num & (1 << k)))
+            count++;
+        else
+            break;
+    }
+
+    return count;
+}
+
+int builtin_ctz(int num)
+{
+    int count = 0;
+    for (size_t k = 0; k < sizeof num * CHAR_BIT; ++k)
+    {
+        if (!(num & (1 << k)))
+            count++;
+        else
+            break;
+    }
+
+    return count;
+}
+
+int builtin_popcount(int num)
+{
+    int count = 0;
+    for (size_t k = 0; k < sizeof num * CHAR_BIT; ++k)
+    {
+        if (num & (1 << k))
+            count++;
+    }
+
+    return count;
+}
+
+bool builtin_parity(int num)
+{
+    enum Parity {EVEN, ODD};
+    bool parity;
+
+    if (!(builtin_popcount(num) & 1))
+        parity = EVEN;
+    else
+        parity = ODD;
+
+    return parity;
+}
+
