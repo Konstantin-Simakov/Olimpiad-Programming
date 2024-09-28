5556329126791f5a76925f316766aaf73fac06fc
Konstantin_Simakov <kvs.2000@mail.ru>
2024-09-28 14:10:32 +0300

    Add files via upload

--- C++/Classwork/CW_2/bitset.cpp
+++ C++/Classwork/CW_2/bitset.cpp
@@ -6,10 +6,11 @@
 using namespace std;
 
 void display_bitset(int x);
+void display_bitset(const bitset<10> & bs);
 
 int main(void)
 {   
-    // 1. Create subset {1, 3, 4, 8} with bitset.
+    // 1. Create set {1, 3, 4, 8} with bitset.
     int x = 0;
     x |= (1 << 1);
     x |= (1 << 3);
@@ -19,8 +20,18 @@ int main(void)
 
     // 2. Set operations.
     int y = (1 << 1) | (1 << 3) | (1 << 4) | (1 << 8);
+    cout << "\nSource set y:\n";
+    display_bitset(y);
     int z = (1 << 3) | (1 << 6) | (1 << 8) | (1 << 9);
+    cout << "Source set z:\n";
+    display_bitset(z);
     int t = y | z;
+    display_bitset(t);
+    t = y & z;
+    display_bitset(t);
+    t = ~y;
+    display_bitset(t);
+    t = y & ~z;
     display_bitset(t);
 
     // 3. Iterate over all subsets of the set {0, 1,..., SET_POWER - 1}.
@@ -42,7 +53,8 @@ int main(void)
     do
     {
         display_bitset(c);
-    } while ((c = (c - x) & x));
+        c = (c - x) & x;
+    } while (c);
 
     // 6. Use bitset from C++ STL.
     bitset<10> bs;
@@ -52,14 +64,28 @@ int main(void)
     bs[7] = 1;
     cout << "\nbs[4] = " << bs[4] << endl;    // 1
     cout << "bs[5] = " << bs[5] << endl;      // 0
-    cout << "Bitset power is " << bs.count() << endl;
+    display_bitset(bs);
+
+    // 7. Use bitwise operations with bitset from C++ STL.
+    bitset<10> ba, bb;
+    ba[2] = 1;
+    ba[3] = 1;
+    ba[4] = 1;
+    bb[2] = 1;
+    bb[5] = 1;
+    bitset<10> bc = ba & bb;
+    bitset<10> bd = ba | bb;
+    display_bitset(ba);
+    display_bitset(bb);
+    display_bitset(bc);
+    display_bitset(bd);
 
     return 0;
 }
 
 void display_bitset(int bitset)
 {
-    cout << "\nThe power of the set is " << __builtin_popcount(bitset) << endl;
+    cout << "The power of the set is " << __builtin_popcount(bitset) << endl;
     if (__builtin_popcount(bitset) > 0)
     {
         cout << "{";
@@ -70,4 +96,22 @@ void display_bitset(int bitset)
     }
     else
         cout << "{<empty set>}\n";
+    cout << endl;
+}
+
+void display_bitset(const bitset<10> & bs)
+{
+    cout << "The power of the set is " << bs.count() << endl;
+    if (bs.count() > 0)
+    {
+        cout << "{";
+        for (int i = 0; i < 10; ++i)
+            if (1 == bs[i])
+                cout << i << ", ";
+        cout << "\b\b}\n";
+
+    }
+    else
+        cout << "{<empty set>}\n";
+    cout << endl;
 }
