681b42c3f00c42a8973dd1bbd2f8bfc172e58b15
Konstantin_Simakov <kvs.2000@mail.ru>
2024-09-29 12:41:36 +0300

    Add files via upload

--- Python/Homework/HW_9/longest_subsequence.py
+++ Python/Homework/HW_9/longest_subsequence.py
@@ -2,39 +2,42 @@
 # Algorithm which calculates the longest subsequence between given words.
 
 
-def get_memtable(word1, word2):
-    rows = len(word1)
-    cols = len(word2)
+def get_memtable(word_1, word_2):
+    """ Construct and get the memoization table. """
+    rows = len(word_1)
+    cols = len(word_2)
 
     # Create the memoization table.
-    memtable = [[0] * (cols + 1) for _ in range(rows + 1)]
+    memtable = [[0] * (cols+1) for _ in range(rows+1)]
 
     # Initialize it.
-    for i in range(1, rows + 1):
-        for j in range(1, cols + 1):
-            if word1[i - 1] == word2[j - 1]:
-                memtable[i][j] = memtable[i - 1][j - 1] + 1
+    for i in range(1, rows+1):
+        for j in range(1, cols+1):
+            if word_1[i-1] == word_2[j-1]:
+                memtable[i][j] = memtable[i-1][j-1] + 1
             else:
-                memtable[i][j] = max(memtable[i - 1][j], memtable[i][j - 1])
+                memtable[i][j] = max(memtable[i-1][j], memtable[i][j-1])
 
     # Display the table for check.
-    for n in range(rows + 1):
+    print()
+    for n in range(rows+1):
         print(*memtable[n])
     print()
 
     return memtable
 
 def get_string(source_word, word):
-    word1 = source_word
-    word2 = word
-    memtable = get_memtable(word1, word2)
+    """ Get the subsequence for the 'word' and 'source_word' as a string. """
+    word_1 = source_word
+    word_2 = word
+    memtable = get_memtable(word_1, word_2)
 
     str = ""
-    i = len(word1)
-    j = len(word2)
+    i = len(word_1)
+    j = len(word_2)
     while i > 0 and j > 0:                                      # Going in reverse order through the table,
-        if word1[i - 1] == word2[j - 1]:                        # If letters in two words matches, add this letter to
-            str += word1[i - 1]                                 # the resulting string.
+        if word_1[i - 1] == word_2[j - 1]:                      # If letters in two words matches, add this letter to
+            str += word_1[i - 1]                                # the resulting string.
             i -= 1
             j -= 1
         elif memtable[i - 1][j] > memtable[i][j - 1]:           # If they don't, and upper value's bigger than left one,
@@ -42,7 +45,7 @@ def get_string(source_word, word):
         else:
             j -= 1                                              # Else, go to the left.
 
-    return ''.join(reversed(str))
+    return "".join(reversed(str))
 
 
 def get_max_subseq(source_word, words_tuple):                   # Returns the longest subsequence.
@@ -58,11 +61,23 @@ def get_max_subseq(source_word, words_tuple):                   # Returns the lo
     return longest_subseq, current_word
 
 
-"""Main code"""
-source = "fosh"
-words = ("fish", "fort")
-longest_subseq, res_word = get_max_subseq(source, words)
+def main():
+    WORDS = ("fish", "fort")
+    source_word = "fosh"
 
-print("The longest subsequence:", longest_subseq)
-print("Its length is", len(longest_subseq))
-print("Probably, the word was", res_word)
+    longest_subseq = ""
+    for word in WORDS:
+        current = get_string(source_word, word)
+        print(current)
+        if len(current) > len(longest_subseq):
+            longest_subseq = current
+            res_word = word
+
+    print("\nThe longest subsequence: ", longest_subseq, ".", sep="")
+    print("Its length is ", len(longest_subseq), ".", sep="")
+    print("Probably, the word was ", res_word, ".", sep="")
+
+
+# Main code.
+if "__main__" == __name__:
+    main()
