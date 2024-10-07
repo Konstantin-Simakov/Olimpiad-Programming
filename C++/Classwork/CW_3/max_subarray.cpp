2e4866b183d1475124abb1a19f1ce79f3c82fb6d
Konstantin_Simakov <kvs.2000@mail.ru>
2024-10-07 12:43:41 +0300

    Add files via upload

---
+++ C++/Classwork/CW_3/max_subarray.cpp
@@ -0,0 +1,69 @@
+// max_subarray.cpp
+// Search for the max sum of a subarray in the array.
+#include <iostream>
+#include <algorithm>        // For max()
+using namespace std;
+
+int max_subarray_1(const int arr[], int n);
+int max_subarray_2(const int arr[], int n);
+int max_subarray_3(const int arr[], int n);
+
+int main(void)
+{
+    const int N = 5;
+    int arr[N] = {-1, 2, -1, 4, 5};
+
+    cout << max_subarray_1(arr, N) << endl;
+    cout << max_subarray_2(arr, N) << endl;
+    cout << max_subarray_3(arr, N) << endl;
+
+    return 0;
+}
+
+int max_subarray_1(const int arr[], int n)
+{
+    int best = 0;
+    for (int a = 0; a < n; ++a)                 // Left side of a subarray.
+    {
+        for (int b = a; b < n; ++b)             // Rigth side of a subarray.
+        {
+            // Calculates the max sum of the subarray.
+            int sum = 0;                    
+            for (int k = a; k <= b; ++k)
+                sum += arr[k];
+            best = max(best, sum);
+        }
+    }
+
+    return best;
+}
+
+int max_subarray_2(const int arr[], int n)
+{
+    int best = 0;
+    for (int a = 0; a < n; ++a)                 // Left side of a subarray.
+    {
+        // Calculate the max sum of a subarray while 
+        // shifting the right boundary of the subarray.
+        int sum = 0;
+        for (int b = a; b < n; ++b)
+        {
+            sum += arr[b];
+            best = max(best, sum);
+        }
+    }
+
+    return best;
+}
+
+int max_subarray_3(const int arr[], int n)
+{
+    int best = 0, sum = 0;
+    for (int k = 0; k < n; ++k)
+    {
+        sum = max(arr[k], sum + arr[k]);
+        best = max(best, sum);
+    }
+
+    return best;
+}
