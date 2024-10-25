cca99f9ef7c3176eb486939321e2900cd2bc9c47
Konstantin_Simakov <kvs.2000@mail.ru>
2024-10-25 14:09:52 +0300

    Add files via upload

--- C++/Classwork/CW_3/max_subarray.cpp
+++ C++/Classwork/CW_3/max_subarray.cpp
@@ -8,11 +8,12 @@ using namespace std;
 int max_subarray_1(const int arr[], int n);
 int max_subarray_2(const int arr[], int n);
 int max_subarray_3(const int arr[], int n);
+void print_subarray(const int * start, const int * end);
 
 int main(void)
 {
-    const int N = 5;
-    int arr[N] = {-10, -9, -6, -8, -5};
+    const int N = 8;
+    int arr[N] = {-1, 2, -4, -3, 5, 8, -5, 6};
 
     cout << max_subarray_1(arr, N) << endl;
     cout << max_subarray_2(arr, N) << endl;
@@ -24,6 +25,8 @@ int main(void)
 int max_subarray_1(const int arr[], int n)
 {
     int best = INT_MIN;
+    int left_index, right_index;                // Indices of the max subarray.
+    
     for (int a = 0; a < n; ++a)                 // Left side of a subarray.
     {
         for (int b = a; b < n; ++b)             // Rigth side of a subarray.
@@ -32,9 +35,15 @@ int max_subarray_1(const int arr[], int n)
             int sum = 0;                    
             for (int k = a; k <= b; ++k)
                 sum += arr[k];
-            best = std::max(best, sum);
+            if (sum > best)
+            {
+                best = sum;
+                left_index = a;
+                right_index = b;
+            }
         }
     }
+    print_subarray(arr + left_index, arr + right_index);
 
     return best;
 }
@@ -67,4 +76,11 @@ int max_subarray_3(const int arr[], int n)
     }
 
     return best;
+}
+
+void print_subarray(const int * start, const int * end)
+{
+    while (start != end)
+        cout << *start++ << ' ';
+    cout << *start << endl;
 }
