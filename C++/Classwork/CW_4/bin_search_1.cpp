0e75fee6e09afa45e2ab03f751f631fd75f23218
Konstantin_Simakov <kvs.2000@mail.ru>
2025-02-22 10:50:31 +0300

    Add files via upload

---
+++ C++/Classwork/CW_4/bin_search_1.cpp
@@ -0,0 +1,43 @@
+// bin_search_1.cpp
+// The first binary search method for an array.
+#include <iostream>
+using namespace std;
+
+// x is the desired element in the array.
+int bin_search(const int arr[], int n, int x);
+
+int main(void)
+{
+    const int SIZE = 5;
+    int arr[SIZE] = {-1, 2, 3, 4, 5};
+    int value = 3;          // Element to search.
+
+    int index = bin_search(arr, SIZE, value);
+    if (-1 == index)
+        cout << "Element " << value << " is not found in the array.\n";
+    else
+        cout << "Element " << value << " is found in the array by index " << index << ".\n"; 
+
+    return 0;
+}
+
+int bin_search(const int arr[], int n, int x)
+{
+    int a = 0, b = n - 1;       // The initial range of search. 
+    int i = -1;                 // The index of the desired element; -1 if element isn't found.
+    while (a <= b)
+    {
+        int k = (a + b) / 2;
+        if (arr[k] == x)
+        {
+            i = k;
+            break;
+        }
+        else if (arr[x] < x)
+            a = k + 1;
+        else
+            b = k - 1;
+    }
+
+    return i;
+}
---
+++ C++/Classwork/CW_4/bin_search_2.cpp
@@ -0,0 +1,36 @@
+// bin_search_2.cpp
+// The second binary search method for an array.
+#include <iostream>
+using namespace std;
+
+// x is the desired element in the array.
+int bin_search(const int arr[], int n, int x);
+
+int main(void)
+{
+    const int SIZE = 5;
+    int arr[SIZE] = {-1, 2, 3, 4, 5};
+    int value = 3;      // Element to search.
+
+    int index = bin_search(arr, SIZE, value);
+    if (-1 == index)
+        cout << "Element " << value << " is not found in the array.\n";
+    else
+        cout << "Element " << value << " is found in the array by index " << index << ".\n"; 
+
+    return 0;
+}
+
+int bin_search(const int arr[], int n, int x)
+{
+    int k = 0;
+    for (int b = n / 2; b >= 1; b /= 2)
+    {
+        while (k + b < n && arr[k + b] <= x)
+            k += b;
+    }
+    if (arr[k] != x)
+        k = -1;         // Element x is not found in the array.
+
+    return k;
+}
